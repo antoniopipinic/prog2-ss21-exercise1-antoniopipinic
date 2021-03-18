7863f7378586863a0e74a330a8c931f76b2d5dc7
antoniopipinic <antonio.pipinic@stud.fh-campuswien.ac.at>
2021-03-18 20:45:12 +0100

    Testcases 8,9 and 10

--- src/main/java/Result.java
+++ src/main/java/Result.java
@@ -17,12 +17,12 @@ public class Result {
         if(grades.isEmpty()){
             return new ArrayList<>();
         }
-        else if (grades.get(ZERO) < MINIMUM_AMOUNT_STUDENTS || grades.get(0) > MAX_AMOUNT_STUDENTS) {
+        else if (grades.get(ZERO) < MINIMUM_AMOUNT_STUDENTS || grades.get(ZERO) > MAX_AMOUNT_STUDENTS) {
             return new ArrayList<>();
         }
 
         for (int i = 1; i <= grades.get(ZERO); i++) {
-            if (grades.get(i) > MAX_GRADES) {
+            if (grades.get(i) > MAX_GRADES || grades.get(i) < ZERO) {
                 result.set(i, ZERO);
 
             } else if(grades.get(i) >= NO_ROUND_MINIMUM && grades.get(i) % DIVIDE_BY_FIVE >= DIVIDE_BY_THREE) {
@@ -31,5 +31,9 @@ public class Result {
             System.out.println(result.get(i));
         }
         return result;
+    }
+
+    public static void main(String[] args) {
+        gradingStudents(List.of(-2, -1, -100));
     }
 }
--- src/test/java/ResultTest.java
+++ src/test/java/ResultTest.java
@@ -25,10 +25,10 @@ public class ResultTest {
     @Test
     @DisplayName("1. Testfall: Ausgangssituation Angabe")
     void testGradingStudents_InitialSpecification() {
-        List<Integer> list1 = Arrays.asList(4, 73, 67, 38, 33);
+        List<Integer> grades = Arrays.asList(4, 73, 67, 38, 33);
         List<Integer> expected = Arrays.asList(4, 75, 67, 40, 33);
         List<Integer> actual
-                = Result.gradingStudents(list1);
+                = Result.gradingStudents(grades);
 
         assertTrue(expected.size() == actual.size()
                 && expected.containsAll(actual)
@@ -38,10 +38,10 @@ public class ResultTest {
     @Test
     @DisplayName("2. Testfall: Rundungen bei 3 Studenten korrekt")
     void testGradingStudents_RoundingUnder40() {
-        List<Integer> list1 = Arrays.asList(3, 38, 39, 40);
+        List<Integer> grades = Arrays.asList(3, 38, 39, 40);
         List<Integer> expected = Arrays.asList(3, 40, 40, 40);
         List<Integer> actual
-                = Result.gradingStudents(list1);
+                = Result.gradingStudents(grades);
 
         assertTrue(expected.size() == actual.size()
                 && expected.containsAll(actual)
@@ -51,9 +51,9 @@ public class ResultTest {
     @Test
     @DisplayName("3. Testfall: Ein Student grade over 100")
     void testGradingStudents_OneStudentGradeOver100() {
-        List<Integer> list1 = Arrays.asList(6, 75, 49, 98, 56, 76, 101);
+        List<Integer> grades = Arrays.asList(6, 75, 49, 98, 56, 76, 101);
         List<Integer> expected = Arrays.asList(6, 75, 50, 100, 56, 76, 0);
-        List<Integer> actual = Result.gradingStudents(list1);
+        List<Integer> actual = Result.gradingStudents(grades);
 
         assertTrue(expected.size() == actual.size()
                 && expected.containsAll(actual)
@@ -63,9 +63,9 @@ public class ResultTest {
     @Test
     @DisplayName("4. Testfall: Zwei Studenten grades over 100")
     void testGradingStudents_TwoStudentsGradesOver100() {
-        List<Integer> list1 = Arrays.asList(6, 75, 49, 98, 150, 76, 101);
+        List<Integer> grades = Arrays.asList(6, 75, 49, 98, 150, 76, 101);
         List<Integer> expected = Arrays.asList(6, 75, 50, 100, 0, 76, 0);
-        List<Integer> actual = Result.gradingStudents(list1);
+        List<Integer> actual = Result.gradingStudents(grades);
 
         assertTrue(expected.size() == actual.size()
                 && expected.containsAll(actual)
@@ -75,9 +75,9 @@ public class ResultTest {
     @Test
     @DisplayName("5. Testfall: Über 61 Studenten ")
     void testGradingStudents_StudentsOver61() {
-        List<Integer> list1 = Arrays.asList(61, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61);
+        List<Integer> grades = Arrays.asList(61, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61);
         List<Integer> expected = new ArrayList<>();
-        List<Integer> actual = Result.gradingStudents(list1);
+        List<Integer> actual = Result.gradingStudents(grades);
 
         assertEquals(expected, actual);
     }
@@ -85,22 +85,55 @@ public class ResultTest {
     @Test
     @DisplayName("6. Testfall: Leere Liste")
     void testGradingStudents_EmptyList() {
-        List<Integer> list1 = new ArrayList<>();
+        List<Integer> grades = new ArrayList<>();
         List<Integer> expected = new ArrayList<>();
-        List<Integer> actual = Result.gradingStudents(list1);
+        List<Integer> actual = Result.gradingStudents(grades);
 
         assertEquals(expected, actual);
     }
 
     @Test
-    @DisplayName("7. Testfall: Eingabe von 0 Studenten")
+    @DisplayName("7. Testfall: Eingabe von 0 Studenten/Noten")
     void testGradingStudents_ZeroStudents() {
-        List<Integer> list1 = Arrays.asList(0,0);
+        List<Integer> grades = Arrays.asList(0,0);
         List<Integer> expected = new ArrayList<>();
-        List<Integer> actual = Result.gradingStudents(list1);
+        List<Integer> actual = Result.gradingStudents(grades);
 
         assertTrue(expected.size() == actual.size()
                 && expected.containsAll(actual)
                 && actual.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("8. Testfall: Eingabe von negativen Punkten")
+    public void testGradingStudents_NegativeGrade() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(2, -1, -100));
+        List<Integer> expected = Arrays.asList(2, 0, 0);
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("9. Testfall: Eingabe von negativen Studenten und negativen Noten")
+    public void testGradingStudents_NegativeStudentsAndNegativeGrades() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(-2, -1, -100));
+        List<Integer> expected = new ArrayList<>();
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
+    }
+
+    @Test
+    @DisplayName("10. Testfall: Eingabe von negativen Studenten")
+    public void testGradingStudents_NegativeStudents() {
+        List<Integer> grades = Result.gradingStudents(Arrays.asList(-2, 50, 100));
+        List<Integer> expected = new ArrayList<>();
+
+        assertTrue(expected.size() == grades.size()
+                && expected.containsAll(grades)
+                && grades.containsAll(expected));
     }
 }
